e15ad5cd6b1fbcd37a6374a52d4555d60bd581ca
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-09-07 15:37:32 +0300

    Update file to add navigation

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,21 +1,37 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
+import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 import * as BsIcon from 'react-icons/bs';
 import './Home.css';
 
 const Home = ({
   name, pic, price, id,
-}) => (
-  <div className="coin">
-    <div className="first-container">
-      <img className="pic" src={pic} alt={name} />
-      <BsIcon.BsFillArrowRightCircleFill className="arrow" id={id} />
-    </div>
-    <div className="second-container">
-      <h2 className="name">{name}</h2>
-      <p className="price">{price}</p>
-    </div>
-  </div>
-);
+}) => {
+  const navigate = useNavigate();
+  const navigateDetails = () => {
+    navigate(`/details/${id}`);
+  };
 
+  return (
+    <section>
+      <div className="coin">
+        <div className="first-container">
+          <img className="pic" src={pic} alt={name} />
+          <BsIcon.BsFillArrowRightCircleFill className="arrow" id={id} onClick={navigateDetails} key={id} />
+        </div>
+        <div className="second-container">
+          <h2 className="name">{name}</h2>
+          <p className="price">{price}</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+Home.propTypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired,
+  pic: PropTypes.node.isRequired,
+};
 export default Home;
